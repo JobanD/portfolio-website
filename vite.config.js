e6f04c975e081255d9a88414139e394b179bf666
JobanD <joban.d555@gmail.com>
2023-01-09 14:36:25 -0500

    Vite config changes for svg rendering

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "@svgr/rollup";
+import svgr from "vite-plugin-svgr";
 import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
